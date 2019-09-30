161d85a5d86a3e64c7d58e80b0bfdf4dd85d0275
282Haniwa <282haniwa@gmail.com>
2019-09-30 20:09:45 +0900

    Install prettier and husky and lint-staged.

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'airbnb'
+    'airbnb',
+    'plugin:prettier/recommended'
   ],
   globals: {
     Atomics: 'readonly',
@@ -20,10 +21,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module'
   },
-  plugins: [
-    'react',
-    'react-hooks'
-  ],
+  plugins: ['react', 'react-hooks'],
   settings: {
     'import/resolver': {
       webpack: {
@@ -32,36 +30,15 @@ module.exports = {
     }
   },
   rules: {
-    'arrow-parens': [
-      'error',
-      'as-needed'
-    ],
-    'no-unused-vars': [
-      'error', { 'ignoreRestSiblings': true }
-    ],
-    'indent': [
-      'error',
-      2,
-      { 'ignoredNodes': ['ConditionalExpression'] }
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'never'
-    ],
-    'no-mixed-spaces-and-tabs': [
-      'error',
-      'smart-tabs'
-    ],
+    'arrow-parens': ['error', 'as-needed'],
+    'no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    indent: ['error', 2, { ignoredNodes: ['ConditionalExpression'] }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
     'comma-style': ['error', 'last'],
-    'comma-spacing': ['error', { 'before': false, 'after': true }],
+    'comma-spacing': ['error', { before: false, after: true }],
     'comma-dangle': ['error', 'never'],
     'no-trailing-spaces': 'error',
     'space-before-blocks': 'error',
@@ -69,27 +46,27 @@ module.exports = {
     'space-unary-ops': 'error',
     'spaced-comment': ['error', 'always'],
     'valid-jsdoc': 'error',
-    'curly': 'error',
+    curly: 'error',
     'dot-notation': 'error',
-    'eqeqeq': ['error', 'always'],
-    'brace-style': ['error', '1tbs', { 'allowSingleLine': true }],
+    eqeqeq: ['error', 'always'],
+    'brace-style': ['error', '1tbs', { allowSingleLine: true }],
     'jsx-quotes': ['error', 'prefer-single'],
     'key-spacing': 'off',
-    'keyword-spacing': ['error', { 'before': true }],
-    'line-comment-position': ['error', { 'position': 'above' }],
+    'keyword-spacing': ['error', { before: true }],
+    'line-comment-position': ['error', { position: 'above' }],
     'lines-between-class-members': ['error', 'always'],
     'no-var': 'error',
-    'max-len': ['error', { 'code': 150 }],
+    'max-len': ['error', { code: 150 }],
     'arrow-spacing': 'error',
     'no-console': 'warn',
-    'no-param-reassign': ['error', { 'props': false }],
+    'no-param-reassign': ['error', { props: false }],
     'no-multi-spaces': [
       'error',
       {
-        'exceptions': {
-          'Property': true,
-          'ImportDeclaration': true,
-          'VariableDeclarator': true
+        exceptions: {
+          Property: true,
+          ImportDeclaration: true,
+          VariableDeclarator: true
         }
       }
     ],
@@ -97,57 +74,55 @@ module.exports = {
       'error',
       'always',
       {
-        'arraysInObjects': true,
-        'objectsInObjects': true
+        arraysInObjects: true,
+        objectsInObjects: true
       }
     ],
-    'object-shorthand': [
-      'error', 'consistent'
-    ],
+    'object-shorthand': ['error', 'consistent'],
     'block-spacing': 'off',
-    'jsx-a11y/anchor-is-valid': ['error', {
-      'components': ['Link'],
-      'specialLink': ['to']
-    }],
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['to']
+      }
+    ],
     'import/prefer-default-export': 'warn',
     'import/no-extraneous-dependencies': [
       'error',
       {
-        'devDependencies': [
-          '**/rollup.config.js',
-          './eslint/tests/*'
-        ]
+        devDependencies: ['**/rollup.config.js', './eslint/tests/*']
       }
     ],
     'react/forbid-prop-types': 'off',
-    'react/jsx-filename-extension': ['error', { 'extensions': ['.jsx'] }],
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx'] }],
     'react/no-direct-mutation-state': 'error',
     'react/no-unused-prop-types': 'off',
     'react/no-unused-state': 'off',
-    'react/sort-prop-types': ['error', { 'ignoreCase': true }],
+    'react/sort-prop-types': ['error', { ignoreCase: true }],
     'react/no-unescaped-entities': 'error',
     'react/jsx-curly-spacing': ['error', 'never'],
     'react/jsx-equals-spacing': ['error', 'never'],
     'react/jsx-first-prop-new-line': ['error', 'multiline-multiprop'],
-    'react/jsx-max-props-per-line': ['warn', { 'maximum': 1, 'when': 'multiline' }],
+    'react/jsx-max-props-per-line': ['warn', { maximum: 1, when: 'multiline' }],
     'react/jsx-one-expression-per-line': 'error',
-    'react/jsx-curly-brace-presence': ['error', { 'props': 'never', 'children': 'ignore' }],
+    'react/jsx-curly-brace-presence': ['error', { props: 'never', children: 'ignore' }],
     'react/jsx-sort-props': [
       'error',
       {
-        'callbacksLast': false,
-        'shorthandFirst': true,
-        'shorthandLast': false,
-        'ignoreCase': true,
-        'noSortAlphabetically': false,
-        'reservedFirst': false
+        callbacksLast: false,
+        shorthandFirst: true,
+        shorthandLast: false,
+        ignoreCase: true,
+        noSortAlphabetically: false,
+        reservedFirst: false
       }
     ],
     'react/jsx-handler-names': [
       'error',
       {
-        'eventHandlerPrefix': 'handle',
-        'eventHandlerPropPrefix': 'on'
+        eventHandlerPrefix: 'handle',
+        eventHandlerPropPrefix: 'on'
       }
     ],
     'react/require-default-props': 'off',
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none'
+}
